6f9021be2e9f78de263e66d177c99e216b5e426e
wenxuan260 <wen-xuan.ma@non.keysight.com>
2025-05-14 11:25:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ from openai import OpenAI
 app = Flask(__name__)
 
 client = OpenAI(
-    #api_key=os.environ.get("MOONSHOT_API_KEY"),  # 推荐使用 Render 的环境变量
-    api_key=sk-MMw06tR25JX52nHURjEiheSw6cV1qTiywnj8X9bsnO1roXnf
+    api_key=os.environ.get("MOONSHOT_API_KEY"),  # 推荐使用 Render 的环境变量
     base_url="https://api.moonshot.cn/v1"
 )
 
